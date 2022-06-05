9ed09b97f0df00b6f97bf52759b5b6b19b5c7c6c
Daniela Farias <farias.danicris@gmail.com>
2022-06-05 15:49:22 -0300

    Tamanho da fonte do banner

--- src/components/Banner/BannerText.tsx
+++ src/components/Banner/BannerText.tsx
@@ -3,10 +3,10 @@ import { Stack, Text } from "@chakra-ui/react";
 export function BannerText() {
   return (
     <Stack maxWidth="32rem" mb={70} ml={["16px", 0]}>
-      <Text fontSize={["2xl", "4xl"]} color="gray.50">
+      <Text fontSize={["3xl", "4xl"]} color="gray.50">
         5 Continentes, infinitas possibilidades.
       </Text>
-      <Text fontSize={["sm", "lg"]} color="gray.100">
+      <Text fontSize={["md", "lg"]} color="gray.100">
         Chegou a hora de tirar do papel a viagem que você sempre sonhou.
       </Text>
     </Stack>
